4b931bf600a81986529041c8b1acb9b764cdf100
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-10-13 19:42:44 -0500

    Cambio 2
    vergruposventa.php
    modificarboleto.php

--- PT-API-P/boletos/modificarBoleto.php
+++ PT-API-P/boletos/modificarBoleto.php
@@ -4,21 +4,44 @@
     require("../BD.php");
     require("../modelo/ClaseBoleto.php"); 
     $conexion = conexion();
-
+    $error = 0;
     $id_boleto = mysqli_real_escape_string($conexion, $_POST['id']);
     $nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
     $descripcion = mysqli_real_escape_string($conexion, $_POST['desc']);
     $inventario = mysqli_real_escape_string($conexion, $_POST['inventario']);
     $precio = mysqli_real_escape_string($conexion, $_POST['precio']);
     
-    $consulta = "UPDATE boleto SET nom_bol = '$nombre', descripcion_boleto = '$descripcion', stock_boleto = '$inventario', stock_act_boleto = '$inventario', stock_repartidor = '$inventario', precio_bol = '$precio', precio_actual_boleto= '$precio' WHERE id_boleto = '$id_boleto'";
-    $registro = mysqli_query($conexion,$consulta) or die(mysqli_error($conexion));   
-    Boleto::EstadoBoleto($id_boleto);
+    $consulta2 = "SELECT stock_boleto, stock_act_boleto, stock_repartidor FROM boleto WHERE id_boleto = '$id_boleto'";
+    $registro2 = mysqli_query($conexion,$consulta2) or die(mysqli_error($conexion));  
+    while ($resultado = mysqli_fetch_array($registro2)){
+        $stock_boleto = $resultado['stock_boleto'];
+        $stock_act_boleto = $resultado['stock_act_boleto'];
+        $stock_repartidor = $resultado['stock_repartidor'];
+    }
+    $stock_repartidor = $stock_repartidor + $inventario;
+    $stock_act_boleto = $stock_act_boleto + $inventario;
+    $stock_boleto = $stock_boleto + $inventario;
+
+    if($stock_repartidor < 0){
+        $error = 1;
+        $stock_repartidor = $stock_repartidor * (-1);
+        $mensaje = "Se excede por ".$stock_repartidor." boletos del stock referente al repartidor";
+    }
+    if($stock_act_boleto < 0){
+        $error = 1;
+        $stock_act_boleto = $stock_act_boleto * (-1);
+        $mensaje = "Se excede por ".$stock_act_boleto." boletos del stock referente a las ventas";
+    }
+    if($error == 0){
+        $consulta = "UPDATE boleto SET nom_bol = '$nombre', descripcion_boleto = '$descripcion', stock_boleto = '$stock_boleto', stock_act_boleto = '$stock_act_boleto', stock_repartidor = '$stock_repartidor', precio_bol = '$precio', precio_actual_boleto= '$precio' WHERE id_boleto = '$id_boleto'";
+        $registro = mysqli_query($conexion,$consulta) or die(mysqli_error($conexion));   
+        Boleto::EstadoBoleto($id_boleto);
+    }
     class Result {}
 
     $response = new Result();
   
-    if(mysqli_error($conexion)){
+    if(){ 
         $response->resultado = 'ERROR';
     }
     else{
---
+++ PT-API-P/usuarios/Vergruposventa.php
@@ -0,0 +1,44 @@
+<?php
+    require("../headers.php");
+    require("../modelo/ClaseVenta.php");
+    require("../modelo/ClaseBoleto.php");
+    require("../modelo/ClaseEvento.php");
+    $id_venta = "";
+    $ventas = Venta::buscarelementosventa($id_venta);
+    if($ventas == null){
+        $numeroventas = count($ventas);
+        $numerodeeventos = 0;
+        for($x = 0; $x<$numeroventas; $x++){
+            $boleto = Boleto::obtenerInfoBoleto($ventas[$x]['fk_boletoele']);
+            while ($while = mysqli_fetch_array($boleto)){
+                $evento_actual = $while["fk_evento_bol"];
+            }
+            if(isset($evento)){
+                $encontrado = 0;
+                for($x = 0; $x<$numerodeeventos; $x++){
+                    $y = $evento[$x]['id'] - $evento_actual;
+                    if($y==0){
+                        $encontrado = 1;
+                        break;
+                    }
+                }
+                if($encontrado == 0){
+                    $evento[$numerodeeventos]['id'] = $evento_actual;
+                    $numerodeeventos++;   
+                }
+            }else{
+                $evento[$numerodeeventos]['id'] = $evento_actual;
+                $numerodeeventos++;  
+            }
+        }
+        for($z = 0; $z<$numerodeeventos; $z++){
+            $datoseve = Evento::obtenerDatosEvento($evento[$z]['id']);
+            $evento[$z]['nombre_evento']=$datoseve['nombre_evento'];
+            $evento[$z]['enlace']=$datoseve['enlace_evento'];
+        }
+        return $evento;//retorna datos del evento para los enlaces
+    }else{
+        $mensaje = "No hay elementos registrados";
+        return $mensaje;
+    }
+?>
